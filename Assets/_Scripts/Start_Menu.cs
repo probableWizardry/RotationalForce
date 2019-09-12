5166d7ef7ed7678b03b93aa7762258b978027ba4
wizardSquid <going.eggward@gmail.com>
2019-09-13 11:06:27 +1200

    Beta 0.1
    It all begins here.

---
+++ Assets/_Scripts/Start_Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Start_Menu : MonoBehaviour
+{
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("I have Quit The Game");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("I have gone to the next scene");
+    }
+}
